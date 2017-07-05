9172912fecddababd7086173c545be84852652a8
Verevka <verevka86@gmail.com>
2017-07-05 08:11:27 +0300

    fix aicp

--- aicp.mk
+++ aicp.mk
@@ -1,12 +1,14 @@
 $(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
-PRODUCT_NAME := lineage_sagit
+PRODUCT_NAME := aicp_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+include vendor/aicp/configs/bootanimation.mk
