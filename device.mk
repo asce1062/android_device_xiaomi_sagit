0e85177b1054ac06215ab4a3296e5c9fbd047878
Verevka <verevka86@gmail.com>
2017-07-27 00:15:47 +0300

    add first_api_level

--- device.mk
+++ device.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Device was launched with N
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.first_api_level=25
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
