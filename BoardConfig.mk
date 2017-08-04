627ac9e619bfe2a61ea916da292b87977258e13a
Verevka <verevka86@gmail.com>
2017-08-04 09:05:46 +0300

    SafetyNet + add the usbcontroller

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,19 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc1b0000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 androidboot.verifiedbootstate=green androidboot.veritymode=enforcing androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := \
+        androidboot.hardware=qcom \
+        ehci-hcd.park=3 \
+        lpm_levels.sleep_disabled=1 \
+        sched_enable_hmp=1 \
+        sched_enable_power_aware=1 \
+        service_locator.enable=1 \
+        swiotlb=2048 \
+    androidboot.usbcontroller=a800000.dwc3 \
+    androidboot.selinux=permissive \
+    androidboot.verifiedbootstate=green \
+    androidboot.veritymode=enforcing \
+    androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -53,6 +65,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8998
 TARGET_KERNEL_CONFIG := msm_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_BUILD_VARIANT := user
 TARGET_PREBUILT_KERNEL := device/xiaomi/sagit/prebuilt/kernel
 
 # Enable real time lockscreen charging current values
