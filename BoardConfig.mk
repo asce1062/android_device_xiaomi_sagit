8a7b8cd5700f138d3d26d64f75e81a315a851815
Verevka <verevka86@gmail.com>
2017-08-07 09:35:44 +0300

    clean cmd-line

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,6 @@ BOARD_KERNEL_CMDLINE := \
         service_locator.enable=1 \
         swiotlb=2048 \
     androidboot.usbcontroller=a800000.dwc3 \
-    androidboot.selinux=permissive \
     androidboot.verifiedbootstate=green \
     androidboot.veritymode=enforcing \
     androidboot.selinux=permissive
