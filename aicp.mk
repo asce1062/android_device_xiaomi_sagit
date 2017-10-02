9aced9823f1fba9c1094bd9df90ed7a173f8156d
Verevka <verevka86@gmail.com>
2017-10-03 00:02:47 +0300

    aicp: build Weather providers

--- aicp.mk
+++ aicp.mk
@@ -36,6 +36,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+# Weather providers
+PRODUCT_PACKAGES += \
+    OpenWeatherMapWeatherProvider \
+    YahooWeatherProvider \
+    WundergroundWeatherProvider
+
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Verevka-dev"
