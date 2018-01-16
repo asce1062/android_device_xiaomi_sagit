2653a240ea5c37ab4c24e4c4e5196324c785c5ed
Verevka <verevka86@gmail.com>
2018-01-16 22:42:10 +0300

    add patches....

---
+++ patches/install.sh
@@ -0,0 +1,17 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="frameworks/av hardware/interfaces"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git apply $rootdirectory/device/xiaomi/sagit/patches/$dir/*.patch
+	echo " "
+done
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory
