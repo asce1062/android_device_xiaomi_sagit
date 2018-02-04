2b3eec48395ba67c8ce353e3381d6b1319b2742e
ShevT <evgeny.shishkov@gmail.com>
2018-02-04 14:35:59 +0300

    Revert "sagit: Enforce RRO on framework-res"
    This reverts commit a06683027011e4bfef227f4ae1458942cfa7c2e6.

--- device.mk
+++ device.mk
@@ -64,9 +64,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
-
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
