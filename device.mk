884a781a131a6182758d0911dff7af32ef8d84fb
ShevT <shev_t@mail.ru>
2018-02-11 12:28:55 +0300

    sagit: add qcom location privapp whitelist and qti low power whitelist
    From 8.1.25 global oreo

--- device.mk
+++ device.mk
@@ -268,6 +268,10 @@ PRODUCT_COPY_FILES += \
 # Powerhint
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Low power Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
