061c7d83816554f0fadf2f768f4e6b0070ec41d0
ShevT <evgeny.shishkov@gmail.com>
2018-03-04 12:05:39 +0300

    hidl: Add android.hardware.memtrack service
    (cherry picked from commit bc7061c88d3124eb691c01885f9ae921232f718b)

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -27,9 +27,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.memtrack@1.0-service \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
