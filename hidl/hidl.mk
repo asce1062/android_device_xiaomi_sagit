266f8c9ff3bfe28facbb3533531af72bec19904c
ShevT <evgeny.shishkov@gmail.com>
2018-03-06 08:02:09 +0300

    sagit: hidl: Add camera provider service

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -16,6 +16,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
