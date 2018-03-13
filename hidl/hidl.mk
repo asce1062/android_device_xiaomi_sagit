d207e6baafdc590aeba5e7f57387dd41aa1484f5
ShevT <evgeny.shishkov@gmail.com>
2018-03-13 19:47:05 +0300

    hidl.mk: Remove android.hidl.manager@1.0-java

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -63,8 +63,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
