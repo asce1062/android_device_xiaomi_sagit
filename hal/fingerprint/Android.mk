0f669aebfe2786116cc4a9723efd2efa93026cb2
ShevT <evgeny.shishkov@gmail.com>
2018-03-14 13:36:57 +0300

    sagit: Changing LOCAL_PROPRIETARY_MODULE to LOCAL_VENDOR_MODULE

--- hal/fingerprint/Android.mk
+++ hal/fingerprint/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.msm8998
 LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.msm8998.rc
-LOCAL_PROPRIETARY_MODULE := true
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
     BiometricsFingerprint.cpp \
