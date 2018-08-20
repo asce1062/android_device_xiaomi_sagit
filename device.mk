ba2ddaa0d6c63bf00935afe2664401bdd6a85ac0
wloot <wlootlxt123@gmail.com>
2018-08-20 17:45:23 +0800

    sagit: Add XiaomiParts

--- device.mk
+++ device.mk
@@ -41,5 +41,9 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+#XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sagit/sagit-vendor.mk)
