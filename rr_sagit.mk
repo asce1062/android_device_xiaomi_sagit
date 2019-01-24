74d4a022cbcf2b3bc34e23ebcd7a23700a579aa2
asce1062 <alex.mbugua@andela.com>
2019-01-24 10:26:39 +0300

    FIx build

--- rr_sagit.mk
+++ rr_sagit.mk
@@ -21,19 +21,15 @@
 
 $(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
 
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/rr/config/gsm.mk)
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # must be before including RR part
 TARGET_BOOTANIMATION_SIZE := 1080p
 
-# Inherit some common rr stuff.
-$(call inherit-product, vendor/rr/config/common.mk)
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 PRODUCT_NAME := rr_sagit
 PRODUCT_DEVICE := sagit
